3c4d94e0b06c99e52093a00ce6ca362a92d8cf2b
Indra Mahkota <indramahkota1@gmail.com>
2021-12-20 20:32:30 +0700

    fix deprecated api substr

--- src/data/utils/Utils.ts
+++ src/data/utils/Utils.ts
@@ -21,7 +21,7 @@ export default class Utils {
 
     static capitalizeWords(text: string): string {
         return text.replace(/\w\S*/g, txt => {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
         });
     }
 }
