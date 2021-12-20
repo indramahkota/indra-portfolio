8858ba949f351f58816b08e1b41e12a8eff9296e
Indra Mahkota <indramahkota1@gmail.com>
2021-12-20 20:00:29 +0700

    add optimization config

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
 const ImageminMozjpeg = require('imagemin-mozjpeg');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const pwaPlugin = require('./pwa/pwa-plugin');
 
@@ -109,6 +110,44 @@ module.exports = () => {
     pwaPlugin.forEach(item => {
       config.plugins.push(item);
     })
+
+    config.devtool = false;
+    config.optimization = {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true
+            },
+            mangle: true,
+            module: false,
+          },
+          extractComments: true
+        }),
+      ],
+      splitChunks: {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 70000,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        automaticNameDelimiter: '~',
+        enforceSizeThreshold: 50000,
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        }
+      }
+    }
   } else {
     config.mode = "development";
     config.entry = path.resolve(__dirname, 'src/index.ts');
