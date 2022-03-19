314ace2d42ac519054331398d215316ab0bca92a
Indra Mahkota <indramahkota1@gmail.com>
2022-03-19 17:37:36 +0700

    add greeting component

--- src/app/views/pages/pageLanding.ts
+++ src/app/views/pages/pageLanding.ts
@@ -1,4 +1,5 @@
 import CommonElement from "@in/base/commonElement";
+import "@in/in-greeting/inGreeting";
 import "@in/in-profile/inProfile";
 import { html } from "lit";
 import { customElement } from "lit/decorators.js";
@@ -7,10 +8,16 @@ import AppConfig from "../../../data/myConfig";
 @customElement("page-landing")
 export class LandingPage extends CommonElement {
   render() {
-    return html`<in-profile
-      .user=${AppConfig.STATIC_USER}
-      .socialMedia=${AppConfig.APP_SOCIAL_MEDIA}
-    ></in-profile>`;
+    return html`
+      <in-greeting
+        greeting=${AppConfig.TEXT_GREETING}
+        description=${AppConfig.TEXT_GREETING_DESC}
+      ></in-greeting>
+      <in-profile
+        .user=${AppConfig.STATIC_USER}
+        .socialMedia=${AppConfig.APP_SOCIAL_MEDIA}
+      ></in-profile>
+    `;
   }
 }
 
