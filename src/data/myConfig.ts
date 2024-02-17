2ee34abc9ae3174d14d8cb4237d41d0d2a731831
Indra Mahkota <indramahkota1@gmail.com>
2024-02-17 15:38:07 +0700

    ♻️ Missing alt

--- src/data/myConfig.ts
+++ src/data/myConfig.ts
@@ -50,7 +50,7 @@ export default class AppConfig {
       url: "#/profile",
       isActive: false,
       imageUrl: userImageUrlRound,
-      imageAlt: undefined,
+      imageAlt: "Profile Image",
     },
   ];
 
